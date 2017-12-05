fc606a80b4fc25e7e96928d0b01574a938153671
Genar Trias Ortiz <genar@acs.li>
2017-12-05 20:59:52 +0100

    Retrieving balances from bittrex, refs #2

--- server/models/balance.js
+++ server/models/balance.js
@@ -1,8 +1,18 @@
+import bittrex from 'node-bittrex-api'
 import fixtures from '../fixtures/balances.json'
 
 module.exports = function (Balance) {
   Balance.getBalances = (cb) => {
-    cb(null, fixtures)
+    const bittrexOpts = Balance.app.get('bittrex')
+    bittrex.options(bittrexOpts)
+
+    bittrex.getbalances(function (data, err) {
+      if (err) {
+        cb(err)
+      }
+
+      cb(null, fixtures)
+    })
   }
 
   Balance.remoteMethod('getBalances', {
